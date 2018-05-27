266c4feed398f3e7c760de68c7a11b4012b44cea
Basheer Malaa <basheer.science@gmail.com>
2018-05-27 09:43:31 -0700

    cleaning up

--- NBCU/ViewControllers/ViewController.swift
+++ NBCU/ViewControllers/ViewController.swift
@@ -12,27 +12,9 @@ import Alamofire
 class ViewController: UIViewController {
     
     @IBOutlet weak var reachabilityLabel: UILabel!
-    
-    private var reachabilityManager = NetworkReachabilityManager()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.reachabilityManager = NetworkReachabilityManager(host: "www.apple.com")
-        
-        self.reachabilityManager?.listener = { [weak self] state in
-            
-            switch state {
-            case .reachable(.ethernetOrWiFi):
-                self?.reachabilityLabel.text = NSLocalizedString("You are online", comment: "")
-            case .notReachable:
-                self?.reachabilityLabel.text = NSLocalizedString("You are offline", comment: "")
-            default:
-                self?.reachabilityLabel.text = NSLocalizedString("You are offline", comment: "")
-                
-            }
-        }
-        
-        self.reachabilityManager?.startListening()
     }
 }
