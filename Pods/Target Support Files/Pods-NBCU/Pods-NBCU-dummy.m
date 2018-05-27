a122842138c79520772e5a632c78cc3019c16a45
Basheer Malaa <basheer.science@gmail.com>
2018-05-27 09:44:57 -0700

    adding additional Cocoapods files

---
+++ Pods/Target Support Files/Pods-NBCU/Pods-NBCU-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NBCU : NSObject
+@end
+@implementation PodsDummy_Pods_NBCU
+@end
---
+++ Pods/Target Support Files/Pods-NBCUTests/Pods-NBCUTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NBCUTests : NSObject
+@end
+@implementation PodsDummy_Pods_NBCUTests
+@end
